20cce6868ddb2c3f65cfc7549eda14effb2627ff
JacobHu0723 <m13137181636@gmail.com>
2021-02-06 23:04:41 +0800

    Create 老板键.cpp

---
+++ 老板键.cpp
@@ -0,0 +1,16 @@
+﻿#include<windows.h>
+
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+
+int main() {
+	bool f = false;
+	HWND hwnd = ::FindWindow(NULL,"计算器");//获取窗口的句柄 
+	while (1) {
+		if (KEY_DOWN(VK_MBUTTON)) { //按下鼠标中键
+			ShowWindow(hwnd, f);
+			f = 1 - f;
+			Sleep(100);
+		}
+		Sleep(20);
+	}
+}
